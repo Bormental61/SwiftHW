62ba76d0f2ca54ef3cb2d3ffaffd89b19d4f2285
Bormental61 <tyranosaurus@yandex.ru>
2023-08-18 14:44:44 +0300

    HW3 modified

---
+++ Swift App HW1/GroupsViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+final class GroupsViewController: UITableViewController {
+
+    override func viewDidload() {
+        super.viewDidload()
+        title = "Groups"
+        view.backgroundColor = .white
+        tableView.backgroundColor = .white
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.barTintColor = .black
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        GroupCell()
+    }
+}
--- Swift App HW1/ViewController.swift
+++ Swift App HW1/ViewController.swift
@@ -1,95 +1,71 @@
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+
+    private lazy var webView: WKWebView = {
+        let webView = WKWebView(frame: view.bounds)
+        webView.navigationDelegate = self
+        return webView
+    }()
     
-    private var imageView = UIImageView(image: UIImage(systemName: "person"))
-
-    private var label: UILabel = {
-        let label = UILabel()
-        label.text = "Log In"
-        label.textAlignment = .center
-        label.font = UIFont(name: "Papyrus", size: 24)
-        label.textColor = .black
-        return label
-    }()
-
-    private var loginField: UITextField = {
-        let login = UITextField()
-        login.borderStyle = .line
-        login.layer.borderWidth = 1
-        login.layer.borderColor = UIColor.black.cgColor
-        login.textColor = .black
-        let placeholderText = NSAttributedString(string: "Login", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
-        login.attributedPlaceholder = placeholderText
-        return login
-    }()
-
-    private var passwordField: UITextField = {
-        let password = UITextField()
-        password.borderStyle = .line
-        password.layer.borderWidth = 1
-        password.layer.borderColor = UIColor.black.cgColor
-        password.textColor = .black
-        let placeholderText = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
-        password.attributedPlaceholder = placeholderText
-        return password
-    }()
-
-    private var enterButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Enter", for: .normal)
-        button.setTiltleColor(.black, for: .normal)
-        return button
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupViews()
+
+        let url = URL(string: "https://oauth.vk.com/autorize?client_id=5464744&redirect_uri=https://oauth.vk.com/blank
+                            .html&scope=262150&display=mobile&responce_type=token")
+        webView.load(URLRequest(url: url!))
     }
 
     private func setupViews() {
-        view.addSubview(imageView)
-        view.addSubview(label)
-        view.addSubview(loginField)
-        view.addSubview(passwordField)
-        view.addSubview(enterButton)
-        setupConstraints()
+        view.addSubview(webView)
     }
 
-    private func setupConstraints() {
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        label.translatesAutoresizingMaskIntoConstraints = false
-        loginField.translatesAutoresizingMaskIntoConstraints = false
-        passwordField.translatesAutoresizingMaskIntoConstraints = false
-        enterButton.translatesAutoresizingMaskIntoConstraints = false
+    private func tap() {
 
-        NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: view.frame.size.width/4),
-            imageView.heightAnchor.constraint(equalToConstant: view.frame.size.width/3.5),
+        let tab1 = UINavigationController(rootViewController: FriendViewController())
+        let tab2 = UINavigationController(rootViewController: GroupsViewController())
+        let tab3 = UINavigationController(rootViewController: PhotosViewController(collectionViewLayout: UICollectionViewFlowLayout()))
 
-            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
-            label.heightAnchor.constraint(equalToConstant: 100),
+        tab1.tabBarItem.title = "Friends"
+        tab2.tabBarItem.title = "Groups"
+        tab3.tabBarItem.title = "Photos"
 
-            loginField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 30),
-            loginField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            loginField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            loginField.heightAnchor.constraint(equalToConstant: 50),
+        let controllers = [tab1, tab2, tab3]
 
-            passwordField.topAnchor.constraint(equalTo: loginField.bottomAnchor, constant: 10),
-            passwordField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            passwordField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            passwordField.heightAnchor.constraint(equalToConstant: 50),
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = controllers
 
-            enterButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 30),
-            enterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            enterButton.widthAnchor.constraint(equalToConstant: view.frame.size.width/4),
-            enterButton.heightAnchor.constraint(equalToConstant: view.frame.size.width/4)
-        ])
+        guard let firstScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                let firstWindow = firstScene.windows.first else {
+                    return
+                }
+        firstWindow.rootViewController = tabBarController
     }
+}
 
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponce: WKNavigationResponce, decisionHandler: @escaping 
+                (WKNavigationResponcePolicy) -> Void) {
+                    guard let url = navigationResponce.responce.url, url.path == "/blank.html", 
+                    let fragment = url.fragment else {
+                        decisionHandler(.allow)
+                        return
+                    }
+                    let params = fragment.componrnts(separatedBy: "&").map { $0.components(separatedBy: "=")}
+                        .reduce([String: String]()){ result, param in 
+                            var dict = result
+                            let key = param[0]
+                            let value = param[1]
+                            dict[key] = value
+                            return dict
+                        }
+                    NetworkService.token = params["access_token"]!
+                    NetworkService.userID = params["user_id"]!
+                    decisionHandler(.cancel)
+                    webView.removeFromSuperview()
+                    tap()
+                }
 }
