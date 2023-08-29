c679c1cfb03188390a6a1b37b5aa45ff12c992e3
Bormental61 <tyranosaurus@yandex.ru>
2023-08-29 14:47:37 +0300

    func updateCell added for GroupCell and PhotoCell

--- Swift App HW1/PhotoCell.swift
+++ Swift App HW1/PhotoCell.swift
@@ -13,6 +13,17 @@ final class PhotoCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+    func updateCell(model: Photo) {
+        DispatchQueue.global().async {
+            if let url = URL(string: model.sizes.first?.url ?? ""), let data = try? Data(contentsOf: url)
+            {
+                DispatchQueue.main.async {
+                    self.photoView.image = UIImage(data: data)
+                }
+            }
+        }
+    }
+
     private func setupViews() {
         addSubview(photoView)
         setupConstraints()
